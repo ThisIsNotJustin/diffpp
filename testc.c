c5e9e1c754e1be923d1ee588efe953721304212e
Justin Coleman <JustinColemanOneTwo@gmail.com>
2025-03-16 08:14:17 -0500

    test diff files

---
+++ testc.c
@@ -0,0 +1,5 @@
+int test1() {
+    printf("Hello World!\n");
+
+    return 0;
+}
---
+++ testcpp.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int test2() {
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
