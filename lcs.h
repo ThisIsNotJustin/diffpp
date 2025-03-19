9f060b5e5f85c98b1c539673175942a4a2af8c07
Justin Coleman <JustinColemanOneTwo@gmail.com>
2025-03-19 09:58:35 -0500

    myers algorithm

--- lcs.h
+++ lcs.h
@@ -21,6 +21,42 @@ std::vector<std::vector<int> > lcs_table(std::vector<T> &a, std::vector<T> &b) {
     return dp;
 }
 
+template <typename T>
+std::vector<std::vector<int> > myers_table(std::vector<T> &a, std::vector<T> &b) {
+    int N = a.size();
+    int M = b.size();
+    int maxD = N + M;
+    std::vector<std::vector<int> > dp(2 * maxD + 1, std::vector<int>(2 * maxD + 1, 0));
+
+    for (int d = 0; d <= maxD; d++) {
+        for (int k = -d; k <= d; k += 2) {
+            int x = 0;
+            if (d == 0) {
+                continue;
+            }
+            
+            if (k == -d || (k != d && dp[d][maxD + k - 1] < dp[d][maxD + k + 1])) {
+                x = dp[d - 1][maxD + k + 1];
+            } else {
+                x = dp[d - 1][maxD + k - 1] + 1;
+            }
+
+            int y = x - k;
+            while (x < N && y < M && a[x] == b[y]) {
+                x++;
+                y++;
+            }
+
+            dp[d][maxD + k] = x;
+            if (x >= N && y >= M) {
+                return dp;
+            }
+        }
+    }
+
+    return dp;
+}
+
 int lcs(std::vector<std::vector<int> > &dp, int a_size, int b_size) {
     return dp[a_size][b_size];
 }
